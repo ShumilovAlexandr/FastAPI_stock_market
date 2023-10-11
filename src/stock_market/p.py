ac928d4cbf68bc0822e0f1f67245826f139ae73d
ShumilovAlexandr <sanya.shumilov1993@mail.ru>
2023-10-12 01:56:20 +0300

    Разобраться с выбросами в тренде

--- src/stock_market/p.py
+++ src/stock_market/p.py
@@ -3,65 +3,48 @@ import pandas as pd
 from prophet import Prophet
 import matplotlib.pyplot as plt
 
-prop = Prophet()
-
-df = pd.read_csv('../new_market.csv')
-
-# Преобразуем столбец с датой в объект datetime
-df['ds'] = pd.DatetimeIndex(df['ds'])
-
-prop.fit(df)
-
-future_price = prop.make_future_dataframe(periods=365)
-forecast = prop.predict(future_price)
-fig = prop.plot(forecast, uncertainty=True)
-fig2 = prop.plot_components(forecast)
-
-
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-# import pandas as pd
-#
-# from prophet import Prophet
-# from prophet.plot import plot_components_plotly, plot_plotly
-#
-#
-# prop = Prophet(yearly_seasonality=True,
-#                weekly_seasonality=True,
-#                seasonality_mode = 'multiplicative')
-#
+# prop = Prophet()
 #
 # df = pd.read_csv('../new_market.csv')
+#
 # # Преобразуем столбец с датой в объект datetime
 # df['ds'] = pd.DatetimeIndex(df['ds'])
 #
 # prop.fit(df)
 #
-# future_dates = prop.make_future_dataframe(periods=12, freq='m')
+# future_price = prop.make_future_dataframe(periods=365)
+# forecast = prop.predict(future_price)
+# fig = prop.plot(forecast, uncertainty=True)
+# fig2 = prop.plot_components(forecast)
 #
-# forecast = prop.predict(future_dates)
 #
-# # # Визуализация прогноза
-# fig = plot_plotly(prop, forecast)
-# fig.update_layout(title_text="Курс акции")
-# fig.show()
+# plt.show()
+df = pd.read_csv('../new_market.csv')
+holidays = pd.read_csv('../holidays.csv')
 
-# # Визуализация компонентов прогнозов
-# fig_components = plot_components_plotly(prop, forecast)
-# fig_components.update_layout(title_text='Компоненты прогнозов')
-# fig_components.show()
+prop = Prophet(holidays=holidays)
+prop.add_country_holidays(country_name='UnitedStates')
+prop.add_seasonality(name='weekly', period=7, fourier_order=5)
+
+# Выбросы, которые выбиваются из общего тренда на графике. Для более
+# "правильного" прогнозирования, их нужно удалить.
+df.loc[(df['ds'] > '2022-10-17') & (df['ds'] < '2022-10-21'), 'y'] = None
+df.loc[(df['ds'] > '2022-10-24') & (df['ds'] < '2022-10-26'), 'y'] = None
+df.loc[(df['ds'] > '2022-11-01') & (df['ds'] < '2022-11-04'), 'y'] = None
+df.loc[(df['ds'] > '2022-11-07') & (df['ds'] < '2022-11-09'), 'y'] = None
 
 
 
-# forecast[["ds", 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+# df.loc[(df['ds'] > '2023-01-01') & (df['ds'] < '2023-02-20'), 'y'] = None
+# df.loc[(df['ds'] > '2023-08-01') & (df['ds'] < '2023-09-25'), 'y'] = None
+
+prop.fit(df)
+
+future_price = prop.make_future_dataframe(periods=365)
+forecast = prop.predict(future_price)
+
+fig = prop.plot(forecast, uncertainty=True)
+fig2 = prop.plot_components(forecast)
+
+plt.show()
 
